2db822ae2e68d69895a171fa0d153cb56da9f8d9
Ramona Łuczkiewicz <ja@agares.info>
2024-12-14 08:59:18 +0100

    Styling for the login page

--- admin/backend/src/templates/user/login.php
+++ admin/backend/src/templates/user/login.php
@@ -1,20 +1,22 @@
 <?php /** @var \Ramona\CMS\Admin\LoginView $model */ ?>
-<form action="" method="POST">
-    <div class="<?= $model->cssModule->classFor('username')?>">
-        <label for="<?= ($usernameId = uniqid('username'))?>">
-            username
-        </label>
-        <input type="text" name="username" id="<?= $usernameId?>"/>
-    </div>
+<div class="<?= $model->cssModule->classFor('login-form')?>">
+    <form action="" method="POST">
+        <div>
+            <label for="<?= ($usernameId = uniqid('username'))?>">
+                username
+            </label>
+            <input type="text" name="username" id="<?= $usernameId?>"/>
+        </div>
 
-    <div>
-        <label for="<?= ($passwordId = uniqid('password'))?>">
-            password
-        </label>
-        <input type="password" name="password" id="<?= $passwordId?>" />
-    </div>
+        <div>
+            <label for="<?= ($passwordId = uniqid('password'))?>">
+                password
+            </label>
+            <input type="password" name="password" id="<?= $passwordId?>" />
+        </div>
 
-    <div>
-        <button type="submit">login</button>
-    </div>
-</form>
+        <div>
+            <button type="submit">login</button>
+        </div>
+    </form>
+</div>
